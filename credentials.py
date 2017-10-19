2ec2442b491f00aace897ea9d06552352af28710
Tuukka Paukkunen <tuukka.paukkunen@cs.helsinki.fi>
2017-10-19 21:15:27 +0300

    Remove Twitter API keys (old keys revoked).

--- credentials.py
+++
@@ -1,6 +0,0 @@
-#these are read only
-
-consumer_key = "HR8j3LQlyX7DwakoyPsgofKWf"
-consumer_secret = "3zjCNMGSU3GXTYTjs0nFpHMDLAlOG0GUJusIy5sTZvs3bdeQTr"
-access_token_key = "921011724999970816-SuQf2lb45UDUFaeRmHdhxFTQ3kCxDHp"
-access_token_secret = "cd2WFU0ieuEUqZoAaXIdb9ftEmcMktLT2lx4qOYvn0W4H"

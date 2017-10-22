c9156bba9804fd9318248aeebf3fb7a877ac64e4
marktuom <marktuom@outlook.com>
2017-10-22 11:51:48 +0300

    Toimiva maini

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-# This file is currently used for testing twitterconnector
+"""Driver module for running the program"""
 
-import twitterconnector
-import tweet_processor
 import sys
-import pandas as pd
+import tweet_processor
+import twitterconnector
+import visualizer
 
 if __name__ == "__main__":
-    #list = twitterconnector.getTweetsAsList(sys.argv[1])
-    tweets = twitterconnector.getTweetsAsList('#Trump')
-    tweets_as_df = pd.DataFrame({'col':list(tweets)})
-    tokenized_tweets = tweet_processor.process_tweets(tweets_as_df)
-    
+
+    # Comment / uncomment to load new tweets
+    twitterconnector.query_tweets_to_file('examples.txt', '#trump', 200)
+    tweets = twitterconnector.read_tweets_from_file('examples.txt')
+    tokenized_tweets = tweet_processor.process_raw_tweets(tweets)
+    counts = tweet_processor.count_tokens(tokenized_tweets)
+    visualizer.word_cloud_from_frequencies(counts, "example_cloud.png")
+
     # TODO get sentiments
-    #pred = classify_tweet()
-    preds = classify_tweets(tokenized_tweets)
-
-
-    # TODO visualize tokenizedDF
-
-    
+    # pred = classify_tweet()
+    # preds = classify_tweets(tokenized_tweets)
